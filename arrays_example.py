53aa690ddb5293aa9779a6dffe8e4e8259fee4cf
calfdog <automationsmith@gmail.com>
2019-04-29 13:53:19 -0400

    Useful batch image tool for resizing your images in a directory to thumbnail or other size

--- arrays_example.py
+++ arrays_example.py
@@ -3,20 +3,21 @@
     of the two numbers such that they add up to a specific target.
 
     Example:
-    Given nums = [2, 7, 11, 15], target = 9,
-    Because nums[0] + nums[1] = 2 + 7 = 9,
-    return [0, 1].
+    Given nums = [7, 5, 11, 4], target = 9,
+    Because nums[1] + nums[3] = 5 + 4 = 9,
+    return [1, 3].
 """
 
-def find_nums(nums, target):
 
+def find_nums(nums, target):
     for x in nums:
         for y in nums:
             if x + y == target:
-                return (nums.index(x), nums.index(y))
+                return nums.index(x), nums.index(y)
 
-# Example
-arr = [2, 7, 11, 15]
-target = 9
-result = find_nums(arr, target)
+
+# Simple example
+arr = [7, 5, 11, 4]
+target_num = 9
+result = find_nums(arr, target_num)
 print(result)
