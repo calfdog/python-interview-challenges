d8dcb67a7451eb6a9b24e844d9a0f11151a6fb98
calfdog <automationsmith@gmail.com>
2019-05-01 15:38:37 -0400

    Enter a range of number and check to see if they are  palindrome - this example starts checking at 10

--- is_palindrome_numbers.py
+++ is_palindrome_numbers.py
@@ -3,13 +3,18 @@
     Developer: Rob Marchetti
 """
 
-def is_palindrome(*args):
+def is_number_palindrome(*args):
     for i in range(*args):
-        num = str(i)
-        if num == num[::-1]:
-            print("This is a palindrome {}", num)
-        else:
-            print("This is not a palindrome {}".format(num))
+        # ignore integers under 10
+        if i >= 10:  # set to another number to change the start
+            # change int to string
+            num = str(i)
+            # if the num matches the num reversed if a palindrome
+            if num == num[::-1]:
+                print("{} is a palindrome".format(num))
+            else:
+                print("{} is not a palindrome".format(num))
 
-# Example
-is_palindrome(1, 99)
+
+# Example - enter start,end (1,99) or just (99)
+is_number_palindrome(1, 99)
