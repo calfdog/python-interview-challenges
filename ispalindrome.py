7325905ab6a76f44be757baa6ac53e9e92abe65b
calfdog <automationsmith@gmail.com>
2019-05-07 06:49:16 -0400

    Added stripping white space and examples

--- ispalindrome.py
+++ ispalindrome.py
@@ -7,6 +7,8 @@ def is_palindrome(s):
     def to_chars(s):
         # make lower case
         s = s.lower()
+        # strip white space
+        s = s.replace(" ", "")
         lowercase_string = ''
 
         # loop thru each character (char) in string and lower it
@@ -21,7 +23,28 @@ def is_palindrome(s):
         if len(s) <= 1:
             return True
         else:
-            # is the first char and last char equal and
+            # is the first char, last char and middle equal
             return s[0] == s[-1] and is_pal(s[1:-1])
-
     return is_pal(to_chars(s))
+
+
+# Examples
+# palindrome
+s1 = "Was iT a Cat I sAw"
+answer1 = is_palindrome(s1)
+print("Is '{}' a palindrome?: {}".format(s1, answer1))
+
+# Not a palindrome
+s2 = "Is it a Cat I see"
+answer2 = is_palindrome(s2)
+print("Is '{}' a palindrome?: {}".format(s2, answer2))
+
+# palindrome
+s1 = "LeVel"
+answer1 = is_palindrome(s1)
+print("Is '{}' a palindrome?: {}".format(s1, answer1))
+
+# Not a palindrome
+s2 = "Ohio"
+answer2 = is_palindrome(s2)
+print("Is '{}' a palindrome?: {}".format(s2, answer2))
